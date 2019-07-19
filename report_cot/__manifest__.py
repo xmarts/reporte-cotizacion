72d5ea2a9e1eadd879d2c22b53970ce6e8c48216
gilxmarts <gilberto.santiago@xmarts.com>
2019-07-19 01:14:01 -0500

    Correcciones

--- report_cot/__manifest__.py
+++ report_cot/__manifest__.py
@@ -10,7 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Xmarts",
+    'Collaborators': "Gilberto SAntiago Acevedo",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
